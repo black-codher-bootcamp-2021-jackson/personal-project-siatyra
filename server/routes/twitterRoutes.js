72d1a620510a23c081d0100103bc37ff0621bd34
siatyra <sia.anglin97@gmail.com>
2023-01-09 22:23:17 +0000

    better security pls

--- server/routes/twitterRoutes.js
+++ server/routes/twitterRoutes.js
@@ -1,22 +1,19 @@
 
 const { default: axios } = require("axios");
-const { response } = require("express");
 
+const lastFiveTweetsUrl = 'https://api.twitter.com/2/users/568545739/tweets?tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=5'
 
 const config = {
     headers: { 
-      'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAJaFYwEAAAAATcAKQ9UVnUH3YzdVqbaUe9cy144%3DwetUPFGUY6rafsx4oNGNRAy3uYE7CTA9EdWTrtvydVsGMNtqtq', 
-      'Cookie': 'guest_id=v1%3A164501591572021451'
+      'Authorization': process.env.TWITTER_BEARER_TOKEN
     }
-  };
-
+};
 
 const twitterRoutes = (app) => {
 
  app.get(`/api/twitter`, async (req, res) => {
-    const latestTweets = await axios.get('https://api.twitter.com/2/users/568545739/tweets?tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=5', config);
+    const latestTweets = await axios.get(lastFiveTweetsUrl, config);
     const latestTweetsData = latestTweets.data;
-    console.log(latestTweetsData)
 
     return res.status(200).send(latestTweetsData);
   });
